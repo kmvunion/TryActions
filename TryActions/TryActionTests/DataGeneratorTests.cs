8742930a22f4567d19b4ae30670a737ab9676727
Nick <kanick@i.ua>
2021-11-28 14:11:08 +0200

    Added tests

---
+++ TryActions/TryActionTests/DataGeneratorTests.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using System;
+using TryActions;
+
+namespace TryActionTests
+{
+    [TestFixture]
+    public class DataGeneratorTests
+    {
+
+        [Test]
+        public void GetGuid_Call_Success()
+        {
+            //Arrange Act
+
+            var result = DataGenerator.GetGuid();
+
+            //Assert
+            var parsResult = Guid.TryParse(result, out Guid resultGuid);
+
+            Assert.IsNotNull(result);
+            Assert.IsTrue(parsResult);
+            Assert.IsFalse(Guid.Equals(Guid.Empty, resultGuid));
+        }
+
+    }
+}
